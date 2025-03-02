ebd4df0f6712949bd9d83e188f5d01c293e00d22
Kirill Kravtsov <killreal777.itmo@gmail.com>
2025-03-02 21:43:18 +0300

    chore(scrapper): update config

--- scrapper/src/main/java/backend/academy/scrapper/ScrapperConfig.java
+++ scrapper/src/main/java/backend/academy/scrapper/ScrapperConfig.java
@@ -1,11 +1,30 @@
 package backend.academy.scrapper;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record ScrapperConfig(@NotEmpty String githubToken, StackOverflowCredentials stackOverflow) {
-    public record StackOverflowCredentials(@NotEmpty String key, @NotEmpty String accessToken) {}
+public record ScrapperConfig(
+        @NotEmpty @DefaultValue("http://localhost:8080") String botApiBase,
+        GitHubConfig gitHub,
+        StackOverflowConfig stackOverflow,
+        SchedulingConfig scheduling) {
+
+    public record GitHubConfig(
+            @NotEmpty @DefaultValue("https://api.github.com") String apiBase, @NotEmpty String token) {}
+
+    public record StackOverflowConfig(
+            @NotEmpty @DefaultValue("https://api.stackexchange.com/2.3") String apiBase, @NotEmpty String key) {}
+
+    public record SchedulingConfig(@NotEmpty String cron, @NotNull Long offsetMinutes) {
+        public SchedulingConfig {
+            if (offsetMinutes <= 0) {
+                throw new IllegalArgumentException("Offset minutes must be greater than 0");
+            }
+        }
+    }
 }
