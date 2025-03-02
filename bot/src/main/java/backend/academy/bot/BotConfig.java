1d52f41fd35479846c1b96f92834ddc8af378373
Kirill Kravtsov <killreal777.itmo@gmail.com>
2025-03-02 21:26:55 +0300

    chore: update bot config

--- bot/src/main/java/backend/academy/bot/BotConfig.java
+++ bot/src/main/java/backend/academy/bot/BotConfig.java
@@ -1,9 +1,19 @@
 package backend.academy.bot;
 
+import com.pengrad.telegrambot.TelegramBot;
 import jakarta.validation.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record BotConfig(@NotEmpty String telegramToken) {}
+public record BotConfig(
+        @NotEmpty String telegramToken, @NotEmpty @DefaultValue("http://localhost:8081") String scrapperApiBase) {
+
+    @Bean
+    public TelegramBot telegramBot() {
+        return new TelegramBot(telegramToken);
+    }
+}
